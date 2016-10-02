ea94578c88716ae958d70cd3db0b82961279f092
PRASHANT CHAURASIA <prashant.chaurasia1311@gmail.com>
2016-10-03 00:57:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,14 @@ element.innerHTML='Hello MADI';
 //move the image when clicked
 
 var img = document.getElementById('madi');
+
+
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft = marginLeft+'px';
+}
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    
+    var interval=setInterval(moveRight,100);
 };
