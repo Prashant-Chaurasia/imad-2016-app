1a56eb060b80e62bfbed1ff3d3c320523aa16f28
PRASHANT CHAURASIA <prashant.chaurasia1311@gmail.com>
2016-10-03 00:50:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,13 @@
 console.log('Loaded!');
+
 //change the main-text
 
 var element=document.getElementById('main_content');
 element.innerHTML='Hello MADI';
+
+//move the image when clicked
+
+var img = getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
